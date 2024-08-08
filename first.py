694b65c613875c6da724182fc95f1f334ca80948
Kevin-waweru <kevinwaweru333@gmail.com>
2024-08-08 22:19:02 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+st.title("data visualization")
+csvfile=st.file_uploader("Upload csv file",'csv')
+
+if csvfile is not None:
+    st.write("file uploaded...")
+
+    df=pd.read_csv(csvfile)
+
+    st.subheader("data preview")
+    st.write(df.head())
+    st.header("data summary")
+    st.write(df.describe())
+
+    st.subheader("filter data")
+    columns=df.columns.tolist()
+    selectedcolumn=st.selectbox("select column to filter by",columns)
+
+    selectedfilter=df[selectedcolumn].unique()
+    selectedvalue=st.selectbox("select value to filter",selectedfilter)
+    filt=df[df[selectedcolumn]==selectedvalue]
+    
+    st.write(filt)
+else:
+    st.write("waiting for file upload...")
