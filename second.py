c9374a507fa3148ab8c9a80d0661d01e51d3b224
Kevin-waweru <kevinwaweru333@gmail.com>
2024-08-09 13:47:11 +0300

    second.py

--- second.py
+++ second.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 import pandas as pd
 
 st.title("data visualization")
@@ -6,8 +7,9 @@ csvfile=st.file_uploader("Upload csv file",'csv')
 
 if csvfile is  None:
     st.write("file uploaded...")
-
-    df=pd.read_csv('https://github.com/Kevin-waweru/first-streamlit/blob/main/second.py')
+    response.get("https://github.com/Kevin-waweru/first-streamlit/blob/main/second.py")
+    csv_content = response.content.decode('utf-8')
+    df=pd.read_csv(pd.compat.StringIO(csv_content)
 
     st.subheader("data preview")
     st.write(df.head())
