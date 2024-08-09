168c42bdde54d5b3dd2a61b0ca0b4c6cbd2d8251
Kevin-waweru <kevinwaweru333@gmail.com>
2024-08-09 14:01:15 +0300

    second.py

--- second.py
+++ second.py
@@ -6,19 +6,9 @@ import io
 st.title("data visualization")
 csvfile=st.file_uploader("Upload csv file",'csv')
 
-if csvfile is  None:
+if csvfile is NOT  None:
     st.write("file uploaded...")
-    repo_url = "second.py"
-
-    response = requests.get(repo_url)
-    csv_content = response.content.decode('utf-8')
-    try:
-     df = pd.read_csv(io.StringIO(csv_content), on_bad_lines='warn')
-     st.write("### Data from GitHub CSV File")
-     st.write(df)
-     st.line_chart(df)
-    except pd.errors.ParserError as e:
-     st.error(f"Failed to parse CSV file: {e}")
+    
 
     st.subheader("data preview")
     st.write(df.head())
